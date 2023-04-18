061c9d2bb6f7d261da2c7de4a2b1ab0be668f434
jemisvachhani <jemisvachhani9494@gmail.com>
2023-04-18 16:30:44 +0530

    jemis

--- nestedif1.c
+++ nestedif1.c
@@ -16,22 +16,9 @@
 #include<stdio.h>
 void main()
 {
-     int number,day_month;
+     int day_month,option;
 
-    printf("enter value day_month");
-    scanf("%d",&number);
-    printf("enter 1 for aries");
-    printf("\n enter 2 for taurus");
-    printf("\n enter 3 for gemini");
-    printf("\n enter 4 for cancer");
-    printf("\n enter 5 for leo");
-    printf("\n enter 6 for virgo");
-    printf("\n enter 7 for libra");
-    printf("\n enter 8 for scorpio");
-    printf("\n enter 9 for sagittarius");
-    printf("\n enter 10 for capricron");
-    printf("\n enter 11 for aquarius");
-    printf("\n enter 12 for pisces");
+    printf("enter day_month");
     scanf("%d",&day_month);
 
 
