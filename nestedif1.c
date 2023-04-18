6cd5d6ff21b12c2969a36abdb2b3ecb829d90fbc
jemisvachhani <jemisvachhani9494@gmail.com>
2023-04-18 15:44:24 +0530

    jemis

---
+++ nestedif1.c
@@ -0,0 +1,40 @@
+//Write a program to display zodiac symbol & given zodiac sign from given birth day and month as per following criteria.
+// march_21 to april_19 = aries
+// april_20 to may_20 =taurus
+// may_21 to june_20 =gemini
+// june_21 to july_22 =cancer
+// july_23 to august_22=leo
+// august_23 to sep_22 = virgo
+// sep_23 to oct_22 = libra
+// oct_23 to nov_21 = scorpio
+// nov_22 to dec_21 = sagittarius
+// dec_22 to jan_19 =capricron
+// jan_20 to feb_18 = aquarius
+// feb_19 to march_20 =pisces
+
+
+#include<stdio.h>
+void main()
+{
+     int number,day_month;
+
+    printf("enter value day_month");
+    scanf("%d",&number);
+    printf("enter 1 for aries");
+    printf("\n enter 2 for taurus");
+    printf("\n enter 3 for gemini");
+    printf("\n enter 4 for cancer");
+    printf("\n enter 5 for leo");
+    printf("\n enter 6 for virgo");
+    printf("\n enter 7 for libra");
+    printf("\n enter 8 for scorpio");
+    printf("\n enter 9 for sagittarius");
+    printf("\n enter 10 for capricron");
+    printf("\n enter 11 for aquarius");
+    printf("\n enter 12 for pisces");
+    scanf("%d",&day_month);
+
+
+
+}
+    
