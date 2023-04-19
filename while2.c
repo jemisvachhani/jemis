8e939a889d7604c27a3f0c6ffe344b37338c56d0
jemisvachhani <jemisvachhani9494@gmail.com>
2023-04-19 20:15:44 +0530

    jemis

---
+++ while2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+     int number = 1;
+
+     while(number < 1001)
+     {
+          printf("%d ", number);
+          number = number + 1;
+     }
+     // printf("%d ", number);
+     // number = number + 1;
+     // printf("%d ", number);
+     // number = number + 1;
+     // printf("%d ", number);
+}
