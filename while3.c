bd5cdedf4ee3bced689e98ebf08d6a0ac078f232
jemisvachhani <jemisvachhani9494@gmail.com>
2023-04-20 13:30:13 +0530

    jemis

---
+++ while3.c
@@ -0,0 +1,32 @@
+//Write a progame to print following pattern 
+//2, 1, 3, 4, 7, 11, 18, 29, 47, 76, 123 .... 300
+
+#include<stdio.h>
+void main()
+{
+    int number=2,ans=3;
+    printf("%d ",ans);
+    while(ans < 300)
+   {
+    number= ans - number;
+    ans = number + ans;
+    printf("%d ",ans);
+   }
+    
+    number= ans - number;
+    ans= number + ans;
+    printf("%d ",ans);
+    
+    
+    number= ans - number;
+    ans= number + ans;
+    printf("%d ",ans);
+    
+    
+    number= ans -number;
+    ans= number+ans;
+    printf("%d ",ans);
+    
+
+
+}
---
+++ while4.c
@@ -0,0 +1,26 @@
+//write a programe to print following pattern
+//0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ... 1000
+
+#include<stdio.h>
+void main()
+{
+    int number=0,ans;
+    printf("%d ",number);
+    
+    number = number + 1;  
+    printf("%d ",number);
+
+    number = number + 2;
+    ans = ans + number; 
+    printf("%d ",number);
+
+    number = number + 3;
+    ans = ans + number;
+    printf("%d ",number);
+    
+    number = number + 4;
+    ans = ans + number;
+    printf("%d ",number);
+
+}
+
