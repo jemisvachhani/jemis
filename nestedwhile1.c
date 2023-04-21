42bdd5ed5e896d146070281ac140513e84de6149
jemisvachhani <jemisvachhani9494@gmail.com>
2023-04-21 12:45:31 +0530

    jemis

--- nestedwhile1.c
+++ nestedwhile1.c
@@ -1,14 +1,64 @@
 //write a programe to print inverted half pyramid
 // https://i1.faceprep.in/fp/articles/img/46684_1580817324.png
+
 #include<stdio.h>
-void mian()
+void main()
 {
-    int count,flash;
-    printf("*");
-    printf("*");
-    printf("*");
-    printf("*");
-    printf("*");
+    int count=0,falsh=0;
+    //printf("*");
+    //printf("*");
+    //printf("*");
+    //printf("*");
+    //printf("*");
+    //printf("*");
+    //printf("\n");
+
+    //while(count < 5 )
+    //{
+     //  printf("*");
+    //   count++;
+    //}
+    //printf("\n");
+    //count=0;
+    //while(count < 4 )
+    //{
+     //   printf("*");
+    //    count++;
+   // }
+    //printf("\n");
+    //count=0;
+    //while(count < 3 )
+  //  {
+    //    printf("*");
+    //    count++;
+   // }
+    while(count)
+    {
+        printf("\n");
+        count=0;
+        while(count< falsh)
+        {
+            printf("*");
+            count=count+1;
+        }
+         falsh=falsh+1;
+          //printf("\n");
+          //count=0;
+         // falsh++;
+
+    }
+     
+    //printf("*"
+    //printf("*");
+    //printf("*");
+    //printf("*");
+
+   
+        
+
+
+
+   
     
     
 }
