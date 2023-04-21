591a95e798876e11af1517e1d2c0002d949903b2
jemisvachhani <jemisvachhani9494@gmail.com>
2023-04-21 19:36:00 +0530

    jemis

---
+++ nestedwhile2.c
@@ -0,0 +1,8 @@
+//write a programe to print inverted half pyramid
+//https://i1.faceprep.in/fp/articles/img/55984_1580817324.png
+#include<stdio.h>
+void main()
+{
+    int count=0;
+    printf()
+}
