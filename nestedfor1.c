8cb5bd264bce69befb7cac0d74bd9af328d2aed1
jemisvachhani <jemisvachhani9494@gmail.com>
2023-04-25 16:23:07 +0530

    jemis

---
+++ nestedfor1.c
@@ -0,0 +1,36 @@
+ //write a programe to print full pyramid
+//https://i1.faceprep.in/fp/articles/img/46684_1580817324.png
+#include<stdio.h>
+void main()
+{
+    int space=0,count=0
+    for (space=0;space<5;space++)
+    {
+        printf("-");
+    }
+       printf("*");
+       printf("\n");
+       printf("-");
+       printf("-");
+       printf("-");
+       printf("-");
+
+       {
+         printf("*");
+         printf("-");
+       }
+       
+       printf("*");
+       printf("\n");
+       
+
+
+
+
+
+
+
+
+
+
+}
