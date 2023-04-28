790bd561d32ec019a9010965c66332ae291a76ee
jemisvachhani <jemisvachhani9494@gmail.com>
2023-04-28 10:13:35 +0530

    jemis

---
+++ array2.c
@@ -0,0 +1,57 @@
+// write a progame to find sum and average for 1 plyer for 5 matches revice.
+
+#include<stdio.h>
+void main()
+{
+    int matche[5],count=0,sum=0,flash=0;
+    float average=0;
+
+      for (count=0;count<5;count++)
+     {
+         printf("Enter a matches %d",count+1);
+         scanf("%d",&matche[count]);
+     }
+     printf("total number of matche are %d ",count);
+      count=flash;
+      //printf("\nThe match is ");
+     for (count=flash;count>=0;count--)
+     
+      {
+        printf("\nThe plyers is match 1 %d",matche[flash]);
+      }
+    //    count=0;
+    //    while(count<5)
+    //     {
+    //        sum=sum+matche[count];
+    //        count++; 
+    //     }
+    //     printf("\nThe value of sum is %d",sum);
+    //      average=sum/5;
+    //     printf("\nThe value of average %f",average);
+
+        // sum=sum+matche[2];
+        // sum=sum+matche[3];
+        // sum=sum+matche[4];
+        // sum=sum+matche[5];
+
+
+       
+    //    printf("\nThe matche is 1 %d", matche[1]);
+    //    printf("\nThe matche is 1 %d", matche[2]);
+    //    printf("\nThe matche is 1 %d", matche[3]);
+    //    printf("\nThe matche is 1 %d", matche[4]);
+
+
+
+    //  printf("Enter a matches 2");
+    //  scanf("%d",&matche[1]);
+    //  printf("Enter a matches 3");
+    //  scanf("%d",&matche[2]);
+    //  printf("Enter a matches 4");
+    //  scanf("%d",&matche[3]);
+    //  printf("Enter a matches 5");
+    //  scanf("%d",&matche[4]);
+
+
+
+}
