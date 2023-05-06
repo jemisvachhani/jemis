57b34d4cf45eec83f7e426cd9726319c63a06721
jemisvachhani <jemisvachhani9494@gmail.com>
2023-05-06 09:37:47 +0530

    jemis

---
+++ function1.c
@@ -0,0 +1,17 @@
+// Write a programe to find bmi using function and also give cateogry
+// https://www.builtlean.com/wp-content/uploads/2013/06/Bmi-chart-1.jpg
+#include <stdio.h>
+float get(float wight)
+{
+    float ans;
+    ans == wight;
+    printf("Enter your wight in kg");
+}
+
+void main()
+{
+
+
+
+    
+}
